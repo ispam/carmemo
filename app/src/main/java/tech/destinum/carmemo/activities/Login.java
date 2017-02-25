5eb29a3b2ab1b28f00e81781082617671e6fc1bc
Diego Fernando Urrea <diegoug@gmail.com>
2017-02-24 21:04:23 -0500

    User Login/signup design completed

--- app/src/main/java/tech/destinum/carmemo/activities/Login.java
+++ app/src/main/java/tech/destinum/carmemo/activities/Login.java
@@ -5,12 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import tech.destinum.carmemo.NavDrawer;
 import tech.destinum.carmemo.R;
 
 public class Login extends AppCompatActivity {
 
     private Button mButton;
+    private TextView mNewAcc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,20 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         mButton = (Button) findViewById(R.id.button_log_in);
+        mNewAcc = (TextView) findViewById(R.id.tvNewAcc);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent();
+                Intent intent = new Intent(getApplicationContext(), NavDrawer.class);
+                startActivity(intent);
+            }
+        });
+
+        mNewAcc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Registration.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/tech/destinum/carmemo/activities/Registration.java
+++ app/src/main/java/tech/destinum/carmemo/activities/Registration.java
@@ -1,15 +1,30 @@
 package tech.destinum.carmemo.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import tech.destinum.carmemo.R;
 
 public class Registration extends AppCompatActivity {
 
+    private TextView mAlreadyReg;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        mAlreadyReg = (TextView) findViewById(R.id.tvAlreadyReg);
+
+        mAlreadyReg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
