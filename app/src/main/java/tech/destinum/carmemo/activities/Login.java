25fa3e8598d8cd52fa063e747624236e55af0f9b
Diego Fernando Urrea <diegoug@gmail.com>
2017-02-25 20:00:11 -0500

    Working on Google Sign in

--- app/src/main/java/tech/destinum/carmemo/activities/Login.java
+++ app/src/main/java/tech/destinum/carmemo/activities/Login.java
@@ -1,11 +1,23 @@
 package tech.destinum.carmemo.activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.auth.api.Auth;
 
 import tech.destinum.carmemo.NavDrawer;
 import tech.destinum.carmemo.R;
@@ -14,6 +26,11 @@ public class Login extends AppCompatActivity {
 
     private Button mButton;
     private TextView mNewAcc;
+    private GoogleApiClient mGoogleApiClient;
+    private GoogleSignInOptions mGoogleSignInOptions;
+    private SignInButton mSignInButton;
+    private int REQUEST_CODE = 1992;
+    private TextView mtext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +39,16 @@ public class Login extends AppCompatActivity {
 
         mButton = (Button) findViewById(R.id.button_log_in);
         mNewAcc = (TextView) findViewById(R.id.tvNewAcc);
+        mSignInButton = (SignInButton) findViewById(R.id.google_button);
+        mtext = (TextView) findViewById(R.id.textView2);
 
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), NavDrawer.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(intent);
+                finish();
             }
         });
 
@@ -38,5 +59,61 @@ public class Login extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        mGoogleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        mGoogleApiClient = new GoogleApiClient.Builder(this).addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
+            @Override
+            public void onConnected(@Nullable Bundle bundle) {
+                goMain();
+            }
+
+            @Override
+            public void onConnectionSuspended(int i) {
+
+            }
+        })
+
+        .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
+            @Override
+            public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                Toast.makeText(getApplicationContext(), "Login Error", Toast.LENGTH_LONG).show();
+                Log.d("Connection Result:", connectionResult.toString());
+            }
+        })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, mGoogleSignInOptions)
+                .build();
+
+        mSignInButton.setSize(SignInButton.SIZE_ICON_ONLY);
+        mSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
+    }
+
+    private void goMain() {
+        Intent intent = new Intent(getApplicationContext(), NavDrawer.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP| Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            GoogleSignInAccount account = result.getSignInAccount();
+            String name = account.getDisplayName();
+            Intent intent = new Intent(getApplicationContext(), NavDrawer.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            intent.putExtra("name", name);
+            mtext.setText(name);
+            startActivity(intent);
+            finish();
+        }
     }
 }
